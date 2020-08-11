5a483da883994033de4c386cb9b719d5d01b3055
shushengritian <1332747149@qq.com>
2020-08-11 18:21:29 +0800

    测试github小绿点

---
+++ src/main/java/com/wanted/onepeople/test/Test.java
@@ -0,0 +1,14 @@
+package com.wanted.onepeople.test;
+
+/**
+ * 描述：测试类
+ * @author xiyouquedongxing
+ * @Date 2020-08-11
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test GitHub");
+    }
+
+}
