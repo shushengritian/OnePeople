907c95ae4809cc50fe4fa40e5dc96b02532f4394
shushengritian <1332747149@qq.com>
2021-03-06 19:31:58 +0800

    jpa提交

---
+++ src/main/java/com/wanted/onepeople/repository/UserDao.java
@@ -0,0 +1,18 @@
+package com.wanted.onepeople.repository;
+
+import com.wanted.onepeople.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * User Dao
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2018-11-07 14:07
+ */
+@Repository
+public interface UserDao extends JpaRepository<User, Long> {
+
+}
