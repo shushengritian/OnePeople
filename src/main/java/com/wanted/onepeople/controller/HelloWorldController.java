052bcaef3724129244e63550ad6f0326257a3732
shushengritian <1332747149@qq.com>
2022-03-28 12:05:13 +0800

    线程

--- src/main/java/com/wanted/onepeople/controller/HelloWorldController.java
+++ src/main/java/com/wanted/onepeople/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ package com.wanted.onepeople.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
+
 /**
  * @author xiyouquedongxing
  * @date 2018/7/23 22:34
@@ -14,6 +16,11 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
     }
 
 }
--- src/main/java/com/wanted/onepeople/learning/thread/ThreadLocalTest.java
+++ src/main/java/com/wanted/onepeople/learning/thread/ThreadLocalTest.java
@@ -1,5 +1,11 @@
 package com.wanted.onepeople.learning.thread;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @Description:
  * @Author fdc
@@ -7,7 +13,30 @@ package com.wanted.onepeople.learning.thread;
  */
 public class ThreadLocalTest {
 
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        ExecutorService executorService2 = Executors.newSingleThreadExecutor();
+        ExecutorService executorService3 = Executors.newCachedThreadPool();
+        ReentrantLock reentrantLock = new ReentrantLock();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+        Semaphore semaphore = new Semaphore(2);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
+        new Thread(() -> {
+            threadLocal.set(Thread.currentThread().getName());
+            //反射获得当前线程的ThreadLocalMap
+            System.out.println(threadLocal.get());
+            countDownLatch.countDown();
+        }).start();
+
+        countDownLatch.await();
+
+        threadLocal.set(Thread.currentThread().getName());
+        //反射获得当前线程的ThreadLocalMap
+        System.out.println(threadLocal.get());
+    }
 
 
 }
